c8d015befa144a907dfbfaa9794fc2041410dd1e
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-08-07 14:30:12 +0100

    Add files via upload

---
+++ Back/Gestion des conges/src/main/java/com/example/gestion_des_conges/Repositories/IEmployeeRepository (1).java
@@ -0,0 +1,16 @@
+package com.example.gestion_des_conges.Repositories;
+
+import com.example.gestion_des_conges.Entities.Employee;
+import com.example.gestion_des_conges.Entities.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IEmployeeRepository extends CrudRepository<Employee, Integer> {
+    Employee findEmployeeByUsername(String username);
+
+    List<Employee> findByRole(Role role);
+
+}
