c5f12af94081bfea9d66ba397b52bd2e9a87f1a5
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-08-07 14:26:15 +0100

    Update IEmployeeServices.java

--- Back/Gestion des conges/src/main/java/com/example/gestion_des_conges/Services/IEmployeeServices.java
+++ Back/Gestion des conges/src/main/java/com/example/gestion_des_conges/Services/IEmployeeServices.java
@@ -5,7 +5,7 @@ import com.example.gestion_des_conges.Entities.Role;
 
 import java.util.List;
 
-public interface IEmployeeServices {
+public interface IEmplyeeServices {
     public Employee addEmployee (Employee employee);
     public Employee updateEmployee(Employee employee);
     public void deleteEmployee(int id);
