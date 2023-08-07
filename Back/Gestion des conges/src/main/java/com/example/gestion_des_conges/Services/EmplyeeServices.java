5d4092e5a5b1bc282d5e66c415e003883e81057a
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-08-07 14:28:54 +0100

    Update EmplyeeServices.java

--- Back/Gestion des conges/src/main/java/com/example/gestion_des_conges/Services/EmplyeeServices.java
+++ Back/Gestion des conges/src/main/java/com/example/gestion_des_conges/Services/EmplyeeServices.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class EmployeeServices implements IEmplyeeServices {
+public class EmplyeeServices implements IEmplyeeServices {
     @Autowired
     private final ICongeRepository congeRepository;
 
