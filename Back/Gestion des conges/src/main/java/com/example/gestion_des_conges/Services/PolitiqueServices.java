b149d7aaf642e1927eb66784551131ebd8777aec
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-08-29 10:32:41 +0100

    Update PolitiqueServices.java

--- Back/Gestion des conges/src/main/java/com/example/gestion_des_conges/Services/PolitiqueServices.java
+++ Back/Gestion des conges/src/main/java/com/example/gestion_des_conges/Services/PolitiqueServices.java
@@ -1,6 +1,6 @@
 package com.example.gestion_des_conges.Services;
 
-import com.example.gestion_des_conges.Entities.Politique;
+import com.example.gestion_des_conges.Entities.*;
 import com.example.gestion_des_conges.Repositories.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +8,31 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PolitiqueServices implements IPolitiqueServices {
+    @Autowired
+    private final ICongeRepository congeRepository;
 
+    @Autowired
+    private final IEmployeeRepository employeeRepository;
+
+    @Autowired
+    private final IJourFerieRepository jourFerieRepository;
+
+    @Autowired
+    private final IMotifRefusRepository motifRefusRepository;
+
+    @Autowired
     private final IPolitiqueRepository politiqueRepository;
 
+    @Autowired
+    private final IRoleRepository roleRepository;
+
+    @Autowired
+    private final ITypeCongeRepository typeCongeRepository;
 
     @Override
     public Politique addPolitique(Politique politique) {
@@ -43,6 +61,23 @@ public class PolitiqueServices implements IPolitiqueServices {
         return politiques;
     }
 
+    @Override
+    public List<Politique> retrieveAllPoitiquesbyJourFerie(int idJourFerie) {
+        List<Politique> allPolitiques = retrieveAllPolitique();
+        return allPolitiques.stream()
+                .filter(politique -> politique.getJourFerie()
+                        .stream()
+                        .anyMatch(jourFerie -> jourFerie.getIdJour() == idJourFerie))
+                .collect(Collectors.toList());
+    }
 
-
+    @Override
+    public List<Politique> retrieveAllPolitiquesByType(int idTypeConge){
+        List<Politique> allPolitiques = retrieveAllPolitique();
+        return allPolitiques.stream()
+                .filter(politique -> politique.getTypeConge()
+                        .stream()
+                        .anyMatch(type -> type.getIdTypeConge() == idTypeConge))
+                .collect(Collectors.toList());
+    }
 }
