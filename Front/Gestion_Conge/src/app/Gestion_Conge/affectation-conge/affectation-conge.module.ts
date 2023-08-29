f658b493a6626693c36b7b2f997d402352040127
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-08-29 10:44:19 +0100

    Update affectation-conge.module.ts

--- Front/Gestion_Conge/src/app/Gestion_Conge/affectation-conge/affectation-conge.module.ts
+++ Front/Gestion_Conge/src/app/Gestion_Conge/affectation-conge/affectation-conge.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AffectationCongeComponent } from './affectation-conge.component';
 import { AffectationCongeRoutingModule } from './affectation-conge-routing.module';
 import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
 import { AutoCompleteModule } from 'primeng/autocomplete';
 import { ChipsModule } from 'primeng/chips';
 import { DropdownModule } from 'primeng/dropdown';
@@ -12,7 +13,22 @@ import { CascadeSelectModule } from 'primeng/cascadeselect';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule } from '@angular/forms';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FileDemoRoutingModule } from 'src/app/demo/components/uikit/file/filedemo-routing.module';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
+import { TooltipModule } from 'primeng/tooltip';
+import { ButtonModule } from 'primeng/button';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { TableModule } from 'primeng/table';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { SidebarModule } from 'primeng/sidebar';
+import { RippleModule } from 'primeng/ripple';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 
 @NgModule({
     imports: [
@@ -28,7 +44,24 @@ import { FormsModule } from '@angular/forms';
 		CascadeSelectModule,
 		MultiSelectModule,
 		InputTextareaModule,
-		InputTextModule
+		InputTextModule,
+		ToggleButtonModule,
+		SelectButtonModule,
+		CheckboxModule,
+		FileDemoRoutingModule,
+		FileUploadModule,
+		ToolbarModule,
+		ToastModule,
+		DialogModule,
+		TooltipModule,
+		ButtonModule,
+		OverlayPanelModule,
+		TableModule,
+		ConfirmDialogModule,
+		SidebarModule,
+		RippleModule,
+		ConfirmPopupModule
+
     ],
     declarations: [AffectationCongeComponent]
 })
