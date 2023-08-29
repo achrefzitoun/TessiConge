16a8524e299b0618aee2857bee3228e7199ba62b
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-08-29 10:40:00 +0100

    Update app.module.ts

--- Front/Gestion_Conge/src/app/app.module.ts
+++ Front/Gestion_Conge/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy, DatePipe } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
@@ -11,27 +11,58 @@ import { EventService } from './demo/service/event.service';
 import { IconService } from './demo/service/icon.service';
 import { NodeService } from './demo/service/node.service';
 import { PhotoService } from './demo/service/photo.service';
-import { ReponseCongeComponent } from './Gestion_Conge/reponse-conge/reponse-conge.component';
-import { AffectationCongeComponent } from './Gestion_Conge/affectation-conge/affectation-conge.component';
+
 import { HttpClientModule } from '@angular/common/http';
-import { DatePipe } from '@angular/common';
+
+import { LoginTessiComponent } from './login-tessi/login-tessi.component';
 
 
-
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { CrudEmployeeComponent } from './crud-employee/crud-employee.component';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+import { RatingModule } from 'primeng/rating';
+import { DialogModule } from 'primeng/dialog';
+import { FileUploadModule } from 'primeng/fileupload';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { DropdownModule } from 'primeng/dropdown';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputNumberModule } from 'primeng/inputnumber';
 @NgModule({
     declarations: [
-        AppComponent, NotfoundComponent
+        AppComponent, NotfoundComponent , LoginTessiComponent, CrudEmployeeComponent
     ],
     imports: [
         AppRoutingModule,
         AppLayoutModule,
-        HttpClientModule,
+        ButtonModule,
+        CheckboxModule,
+        InputTextModule,
+        FormsModule,
+        PasswordModule,
+        RippleModule,
+        TableModule,
+        ToastModule,
+        ToolbarModule,
+        RatingModule,
+        DialogModule,
+        FileUploadModule,
+        InputTextareaModule,
+        DropdownModule,
+        RadioButtonModule,
+        InputNumberModule
 
     ],
     providers: [
         { provide: LocationStrategy, useClass: HashLocationStrategy },
         CountryService, CustomerService, EventService, IconService, NodeService,
-        PhotoService, ProductService , DatePipe 
+        PhotoService, ProductService, DatePipe
     ],
     bootstrap: [AppComponent]
 })
