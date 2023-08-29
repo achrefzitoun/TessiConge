7eba152782b30ecafb9ffa65cb80368f33e667b7
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-08-29 10:36:08 +0100

    Update app-routing.module.ts

--- Front/Gestion_Conge/src/app/app-routing.module.ts
+++ Front/Gestion_Conge/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { AppLayoutComponent } from "./layout/app.layout.component";
-import { RouterModule } from '@angular/router';
+import { LoginTessiComponent } from './login-tessi/login-tessi.component';
+import { CrudEmployeeComponent } from './crud-employee/crud-employee.component';
 
 @NgModule({
     imports: [
@@ -20,11 +22,13 @@ import { RouterModule } from '@angular/router';
                     { path: 'gestionroles', loadChildren: () => import('./Gestion_Roles/gestion-role.module').then(m => m.GestionRole) },
                 ]
             },
-            
-            
+            { path: 'gestionconge', loadChildren: () => import('./Gestion_Conge/gestion-conge.module').then(m => m.GestionConge) },
+            { path: 'gestionroles', loadChildren: () => import('./Gestion_Roles/gestion-role.module').then(m => m.GestionRole) },
             { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
             { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
             { path: 'notfound', component: NotfoundComponent },
+            { path: 'login', component: LoginTessiComponent},
+            { path: 'crudEmployee', component: CrudEmployeeComponent},
             { path: '**', redirectTo: '/notfound' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
     ],
