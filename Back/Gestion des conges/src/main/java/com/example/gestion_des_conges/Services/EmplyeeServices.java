e9e67bce9c1dc2f56c56767c2c0af1a05dcb6fd7
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-08-29 10:29:13 +0100

    Update EmplyeeServices.java

--- Back/Gestion des conges/src/main/java/com/example/gestion_des_conges/Services/EmplyeeServices.java
+++ Back/Gestion des conges/src/main/java/com/example/gestion_des_conges/Services/EmplyeeServices.java
@@ -11,11 +11,47 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class EmplyeeServices implements IEmplyeeServices {
+public class EmployeeServices implements IEmplyeeServices {
+    @Autowired
+    private final ICongeRepository congeRepository;
 
-
+    @Autowired
     private final IEmployeeRepository employeeRepository;
 
+    @Autowired
+    private final IJourFerieRepository jourFerieRepository;
+
+    @Autowired
+    private final IMotifRefusRepository motifRefusRepository;
+
+    @Autowired
+    private final IPolitiqueRepository politiqueRepository;
+
+    @Autowired
+    private final IRoleRepository roleRepository;
+
+    @Autowired
+    private final ITypeCongeRepository typeCongeRepository;
+
+    @Override
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public Employee retrieveEmployee(int id) {
+        return null;
+    }
 
     @Override
     public List<Employee> retrieveAllEmployee() {
@@ -33,7 +69,5 @@ public class EmplyeeServices implements IEmplyeeServices {
     public Employee getEmployeById(Integer id){
         return employeeRepository.findById(id).orElse(null);
     }
-
-
 
 }
