78bbe406c551cfb3815b815562a53139af0fcf54
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-08-29 10:37:45 +0100

    Update conge-services.service.ts

--- Front/Gestion_Conge/src/app/Services/Conge/conge-services.service.ts
+++ Front/Gestion_Conge/src/app/Services/Conge/conge-services.service.ts
@@ -15,7 +15,7 @@ export class CongeServicesService {
 
   private apiUrl = 'http://localhost:8081/Tessi/conge';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private datePipe: DatePipe) { }
 
   getAllEmployees(): Observable<Employee[]> {
     return this.http.get<Employee[]>(`${this.apiUrl}/GetAllEmplyees`);
@@ -56,6 +56,129 @@ export class CongeServicesService {
     return this.http.put<any>(`${this.apiUrl}/getAutorisation`, autorisation);
 
   }
+
+    
+  getAllConges() {
+    return this.http.get<Conge[]>("http://localhost:8081/Tessi/demande/allConge");
+  }
+
+  getAllCongesEnAttente() {
+    return this.http.get<Conge[]>("http://localhost:8081/Tessi/demande/CongeEnAttente");
+  }
+
+  
+  getCongesByEmploye() {
+    return this.http.get<Conge[]>("http://localhost:8081/Tessi/demande/allCongeEmployee");
+  }
+
+  getOneConge(id: number) {
+    return this.http.get<Conge>(`http://localhost:8081/Tessi/demande/getConge?id=${id}`);
+  }
+
+  addConge(conge: Conge, file: File, idtype: number, startDate: string, endDate: string): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('idtype', idtype.toString());
+    formData.append('startDate', startDate.toString());
+    formData.append('endDate', endDate.toString());
+    formData.append('conge', JSON.stringify(conge));
+    return this.http.put<any>('http://localhost:8081/Tessi/demande/addAndAssignType', formData);
+  }
+
+  updateConge(conge: Conge, file: File, idtype: number, startDate: string, endDate: string) {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('idtype', idtype.toString());
+    formData.append('startDate', startDate.toString());
+    formData.append('endDate', endDate.toString());
+    formData.append('conge', JSON.stringify(conge));
+    return this.http.put('http://localhost:8081/Tessi/demande/updateConge', formData, { responseType: 'text' });
+  }
+
+
+  annulConge(id: number) {
+    return this.http.put(`http://localhost:8081/Tessi/demande/annulCong?id=${id}`, {});
+  }
+
+
+  getAllType(): Observable<TypeConge[]> {
+    return this.http.get<TypeConge[]>("http://localhost:8081/Tessi/demande/allTypeConge");
+  }
+
+
+  getDurationN(startDate: String, endDate: String) {
+
+    const params = new HttpParams()
+      .set('startDate', startDate.toString())
+      .set('endDate', endDate.toString());
+
+    return this.http.get<number>("http://localhost:8081/Tessi/demande/durationN", { params });
+  }
+
+  getDurationS(startDate: String, idtype: number) {
+
+    const params = new HttpParams()
+      .set('startDate', startDate.toString())
+      .set('idtype', idtype.toString());
+
+    return this.http.get<number>("http://localhost:8081/Tessi/demande/durationS", { params });
+  }
+
+  getTypeCongebyNature(natureType: NatureType): Observable<TypeConge[]> {
+    return this.http.get<TypeConge[]>("http://localhost:8081/Tessi/demande/getTypeCongebyNature/" + natureType);
+  }
+
+  getEmployeesDelegDev() {
+    return this.http.get<Employee[]>("http://localhost:8081/Tessi/demande/getemployeedelegDev");
+  }
+
+  getEmployeesDelegContr() {
+    return this.http.get<Employee[]>("http://localhost:8081/Tessi/demande/getemployeedelegContr");
+  }
+
+  delegationRole(idEmpl: number, startDate: string, endDate: string) {
+    const formData = new FormData();
+    formData.append('idEmpl', idEmpl.toString());
+    formData.append('startDate', startDate.toString());
+    formData.append('endDate', endDate.toString());
+    return this.http.put<any>('http://localhost:8081/Tessi/demande/delegation', formData);
+
+  }
+
+  exportCongeExcel(): Observable<HttpResponse<Blob>> {
+    const headers = new HttpHeaders({ 'Accept': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    return this.http.get<Blob>('http://localhost:8081/Tessi/demande/exportExcel', { headers: headers, observe: 'response', responseType: 'blob' as 'json' });
+  }
+
+  getGeneratedPdf() {
+
+    return this.http.get('http://localhost:8081/Tessi/demande/generatepdf', { responseType: 'blob' });
+  }
+
+  getAllCongeAccepr() {
+    return this.http.get<Conge[]>("http://localhost:8081/Tessi/demande/getAllCongesAccep");
+  }
+
+  getEmployesBetweenDates(startDate: string, endDate: string): Observable<Employee[]> {
+    const url = `http://localhost:8081/Tessi/demande/getEmployesBetweenDates?startDate=${startDate}&endDate=${endDate}`;
+    return this.http.get<Employee[]>(url);
+  }
+
+  getEmplyesContainDate(startDate: string): Observable<Employee[]> {
+    const url = `http://localhost:8081/Tessi/demande/getEmplyesContainDate?startDate=${startDate}`;
+    return this.http.get<Employee[]>(url);
+  }
+
+  getCongesSuperviseurs(): Observable<Conge[]> {
+    return this.http.get<Conge[]>("http://localhost:8081/Tessi/demande/getListCongeDesSuperviseurs");
+  }
+
+  
+  getDelegueEmployeeName(idConge: number): Observable<string> {
+    const url = `http://localhost:8081/Tessi/demande/getDelegueEmployeeName?idConge=${idConge}`;
+    return this.http.get(url, { responseType: 'text' }); // Définit le type de réponse comme 'text'
+  }
+
 
 
 }
